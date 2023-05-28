510769362e59ff185f44f2994c71b2f656ddb0b6
song <1311911048@qq.com>
2023-05-29 03:01:31 +0800

    修改node-lesson

--- node-lesson/lesson7/vendor/tests.js
+++
@@ -1,8 +0,0 @@
-'use strcit';
-
-var should = chai.should();
-describe('simple test', function () {
-  it('should equal 0 when n === 0', function () {
-    window.fibonacci(0).should.equal(0);
-  });
-});
